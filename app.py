b24a0c267062a0c53ae0d7d9820aa402be197995
ramirezg <gabriel.ramirez@gsa.gov>
2015-10-10 20:30:30 -0400

    add app scheduler

--- app.py
+++ app.py
@@ -1,20 +1,12 @@
 import os
-import json
+import logging
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import jsonify, render_template, Flask
 from TweetNet import TweetNet
 
+# Import Flask app
 app = Flask(__name__)
-
-mongo_uri = 'mongodb://%s:27017/' % os.environ[
-    'TWEETSERVER_DB_1_PORT_27017_TCP_ADDR']
-
-# start client get tweets
-client = TweetNet(
-    query='foia',
-    collection='foia',
-    database='twitter',
-    mongo_uri=mongo_uri)
 
 
 @app.route('/<keyword>')
@@ -22,22 +14,31 @@ def graph(keyword):
     return render_template('graph.html', keyword=keyword)
 
 
-@app.route('/api/collect/')
-def get():
-    client.get_tweets()
-    return "Collecting Tweet"
-
-
-@app.route('/api/drop')
-def drop():
-    client.drop_collection()
-    return "Collection Dropped"
-
-
 @app.route('/api/search/<keyword>')
 def search(keyword):
     data = client.prepare_graph(keyword)
     return jsonify(data)
 
+
 if __name__ == "__main__":
+    # Setup logging
+    logging.basicConfig(level=logging.INFO)
+
+    # Get Mongo URI
+    mongo_uri = 'mongodb://%s:27017/' % os.getenv(
+        'TWEETSERVER_DB_1_PORT_27017_TCP_ADDR', 'localhost')
+
+    # Start tweet collector
+    client = TweetNet(
+        query=os.getenv('TWEET_QUERY'),
+        collection='latino',
+        database='twitter',
+        mongo_uri=mongo_uri)
+
+    # Schedule Updates
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(client.get_tweets, 'interval', minutes=30)
+    scheduler.start()
+
+    # Start app
     app.run(host='0.0.0.0', debug=True, use_reloader=False)
